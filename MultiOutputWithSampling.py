7f2d289a383f5ef3de37ad35593eccb34ce6f49c
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-11-23 18:09:47 -0500

    Pickle using pipe

--- MultiOutputWithSampling.py
+++ MultiOutputWithSampling.py
@@ -1,6 +1,21 @@
 import numpy as np
 from sklearn.base import clone
 
+import logging
+import pickle
+from multiprocessing import Process
+from pathlib import Path
+import os
+import sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
+def _mp_fit(model, X_train, y_train, id_, i):
+    logging.debug("Start to fit label {}".format(i))
+    model.fit(X_train, y_train)
+    pickle.dump(model, open(Path('mp_{}.pkl'.format(id_)), 'wb'))
+
 
 class MultiOutputWithSampling:
     def __init__(self, model):
@@ -9,14 +24,29 @@ class MultiOutputWithSampling:
 
     def fit(self, X_train, y_train):
         v = y_train.shape[1]
-        self.list_ = [clone(self.model) for _ in range(v)]
+
+        # Implement Multiprocess to fasten the training
+        record, ids = [], []
         for i in range(v):
-            self.list_[i].fit(X_train, y_train[:, i])
+            model_ = clone(self.model)
+            ids.append("{}_{}".format(i, id(model_)))
+            process = Process(target=_mp_fit, args=(model_, X_train, y_train[:, i], ids[i], i))
+            record.append(process)
+            process.start()
+
+        for process in record:
+            process.join()
+
+        self.list_ = [None for _ in range(v)]
+        for i in range(v):
+            file_path = 'mp_{}.pkl'.format(ids[i])
+            self.list_[i] = pickle.load(open(file_path, 'rb'))
+            os.remove(file_path)
 
     def predict(self, X_test):
         n = X_test.shape[0]
         v = len(self.list_)
         y_pred = np.zeros((n, v))
         for i in range(v):
-            y_pred[:i] = self.list_[i].predict(X_test)
+            y_pred[:, i] = self.list_[i].predict(X_test)
         return y_pred
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.multioutput import MultiOutputClassifier
 
 from preprocess import load_and_process, pca
 from metrics import eval_model
+from MultiOutputWithSampling import MultiOutputWithSampling
 
 
 def main():
@@ -18,6 +19,8 @@ def main():
     # Linear Model
     eval_model(MultiOutputClassifier(LogisticRegression(max_iter=1e4), n_jobs=-1), train_features, y_train,
                id_='lr_no_sampling')
+    eval_model(MultiOutputWithSampling(LogisticRegression(max_iter=1e4)), train_features,
+               y_train, id_='demo_lr')
 
 
 if __name__ == '__main__':
