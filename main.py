54f17b22a83c24ec7c1e7c9a2a24f366da78d115
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-11-23 19:02:27 -0500

    Add RandomForest

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import sys
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
 
 from preprocess import load_and_process, pca
@@ -21,6 +22,9 @@ def main():
                id_='lr_no_sampling')
     eval_model(MultiOutputWithSampling(LogisticRegression(max_iter=1e4)), train_features,
                y_train, id_='demo_lr')
+    eval_model(MultiOutputWithSampling(
+        RandomForestClassifier(n_estimators=200, max_depth=10, random_state=43, min_samples_split=10)), train_features,
+        y_train, id_='rf_200_10_43_10_no_sampling')
 
 
 if __name__ == '__main__':
